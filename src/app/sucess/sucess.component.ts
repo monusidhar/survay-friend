a411ef3dea1407aaeda5be1f8b0b9e2260c9661a
Jagdeep <mohit@izap.in>
2020-11-17 13:32:48 +0530

    chnages

--- src/app/sucess/sucess.component.ts
+++ src/app/sucess/sucess.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { GeneratorService } from '../services/generator.service';
 
 @Component({
@@ -13,11 +13,20 @@ export class SucessComponent implements OnInit {
   ngOnInit(): void {
     this.generatorService.token.subscribe(res => {
       this.id= res;
-      debugger
       console.log('this is id'+res);
-      debugger
     });
-    debugger
   }
-
+  copyMessage(val: string){
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = val;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+  }
 }
